e1abe50d4ff317b0d3fa72a00b5a1b4e4c933a58
Bálint Antal <antalbalint97@gmail.com>
2025-10-16 20:16:37 +0200

    Removed embedding and search_reviews Lambda modules from repo, kept locally

--- terraform/modules/lambda/embedding/outputs.tf
+++
@@ -1,8 +0,0 @@
-#############################################
-# Outputs
-#############################################
-
-output "lambda_arn" {
-  description = "ARN of the embedding Lambda function"
-  value       = aws_lambda_function.embed.arn
-}
--- terraform/modules/lambda/search_reviews/outputs.tf
+++
@@ -1,8 +0,0 @@
-#############################################
-# Outputs
-#############################################
-
-output "lambda_arn" {
-  description = "The ARN of the search_reviews Lambda function"
-  value       = aws_lambda_function.search_reviews.arn
-}
